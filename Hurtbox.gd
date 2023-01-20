195768efa18ee80a72c96577aef53ffc241b8b7d
ShellGoldfish0 <jjsapp05@gmail.com>
2023-01-20 12:58:26 -0500

    help

--- Hurtbox.gd
+++ Hurtbox.gd
@@ -1,9 +1,17 @@
 extends Area2D
+<<<<<<< HEAD
 
 var invincible = false setget set_invincible
 
 onready var timer = $Timer
 onready var collisionShape = $CollisionShape2D
+=======
+class_name Hurtbox
+
+var kb_d 
+var kb_f = 3
+var dam = 10
+>>>>>>> parent of 8acb496 (Minor patch)
 
 signal invincibility_started
 signal invincibility_ended
@@ -22,8 +30,20 @@ func start_invincibility(duration):
 func _on_Timer_timeout():
 	self.invincible = false
 
+<<<<<<< HEAD
 func _on_Hurtbox_invincibility_started():
 	collisionShape.set_deferred("disabled", true)
 
 func _on_Hurtbox_invincibility_ended():
 	collisionShape.disabled = false
+=======
+func _on_area_entered(hitbox: Hitbox) -> void:
+	if Hitbox == null:
+		return
+	if owner.has_method("take_damage"):
+		print("killed")
+		print(kb_f)
+		#kb_d = get_global_mouse_position() - global_position.normalized()
+		owner.take_damage(dam)
+		print(owner.health)
+>>>>>>> parent of 8acb496 (Minor patch)
