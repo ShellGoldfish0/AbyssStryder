9d3b49fe34e8399d417ed5c717a5dc5858de6e4f
slackerslab <slackerslab05@gmail.com>
2023-04-14 13:10:57 -0400

    0.0.6.4
    wetroom menu added

---
+++ Important_Scenes/Reusable/pauseMenu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+var is_paused = false setget set_is_paused
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+			self.is_paused =!is_paused
+			
+
+func set_is_paused(value):
+	is_paused = value
+	get_tree().paused = is_paused
+	visible = is_paused
+
+
+
+#when the pause button is pressed we will get the scene tree and
+# set the paused to true
+
+func _on_Resume_pressed():
+	self.is_paused = false
+
+
+#when the resume button is pressed we will get the scene tree and
+# set the paused to false
+	
+func _on_Restart_pressed():
+	get_tree().reload_current_scene()
